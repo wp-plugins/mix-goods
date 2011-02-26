ca43bf417fb4aace70b35f245696eb72c2bd4ca3
mixmarket <mixmarket@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-02-26 19:59:38 +0000

    git-svn-id: https://plugins.svn.wordpress.org/mix-goods/trunk@352014 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- mixgoods.php
+++ mixgoods.php
@@ -6,7 +6,7 @@
 /*
 Plugin Name: Mix-Goods
 Plugin URI: http://www.mixmarket.biz/doc/partners/goods/programs/?from=wordpress
-Description: Плагин для быстрой интеграции рекламных кодов Микс-Товары Партнерской сети Миксмаркет. Для блока "Где купить" нужно указать [gdekupit gk_id="номер товарной группы" brand="бренд" model="модель"] а также при необходимости другие параметры: type="vert или hor" - тип показа; pagesize="количество ссылок на одну страницу". Для блока "Контекстный товар" нужно указать [kt kt_id="номер товарной групп"], а можно указать ID нужной категории (cat_id="XXX"). Все настройки нужно производить в <a href=http://www.mixmarket.biz/doc/partners/goods/programs/>кабинете партнера</a>
+Description: Плагин для быстрой интеграции рекламных кодов Микс-Товары Партнерской сети Миксмаркет. Для блока "Где купить" нужно указать [gdekupit gk_id="номер товарной группы" brand="бренд" model="модель"] а также при необходимости другие параметры: type="vert или hor" - тип показа; pagesize="количество ссылок на одну страницу". Для блока "Контекстный товар" нужно указать [kt kt_id="номер товарной групп"], а можно указать ID нужной категории (cat_id="XXX"). Все настройки нужно производить в <a href=http://www.mixmarket.biz/doc/partners/goods/programs/?from=wordpress>кабинете партнера</a>
 Author:  Партнерская сеть Миксмаркет 
 Version: 1.0
 Author URI: http://www.mixmarket.biz/doc/partners/goods/programs/?from=wordpress
--- readme.txt
+++ readme.txt
@@ -7,7 +7,6 @@ Tested up to: 3.1
 Stable tag: 2.5
 License: GPLv2 or later
 
-Quick and easy installation of advertising Mix-Goods
 Плагин для быстрой и простой установки рекламных кодов Микс-Товары
 
 == Description ==
@@ -20,7 +19,7 @@ Quick and easy installation of advertising Mix-Goods
 Upload the Mix-Goods plugin to your blog, Activate it.
 Для блока "Где купить" нужно указать [gdekupit gk_id="номер товарной группы" brand="бренд" model="модель"] а также при необходимости другие параметры: type="vert или hor" - тип показа; pagesize="количество ссылок на одну страницу". 
 Для блока "Контекстный товар" нужно указать [kt kt_id="номер товарной групп"], а можно указать ID нужной категории (cat_id="XXX"). 
-Все настройки нужно производить в <a href=http://www.mixmarket.biz/doc/partners/goods/programs/>кабинете партнера</a>
+Все настройки нужно производить в <a href=http://www.mixmarket.biz/doc/partners/goods/programs/?from=wordpress>кабинете партнера</a>
 
 
 == Changelog ==
