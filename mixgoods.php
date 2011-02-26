bbf56aada9c6befa276232723f04c28726c27f47
mixmarket <mixmarket@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-02-26 19:54:16 +0000

    git-svn-id: https://plugins.svn.wordpress.org/mix-goods/trunk@352006 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ mixgoods.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * @package Mix-Goods
+ * @version 1.0
+ */
+/*
+Plugin Name: Mix-Goods
+Plugin URI: http://www.mixmarket.biz/doc/partners/goods/programs/?from=wordpress
+Description: Плагин для быстрой интеграции рекламных кодов Микс-Товары Партнерской сети Миксмаркет. Для блока "Где купить" нужно указать [gdekupit gk_id="номер товарной группы" brand="бренд" model="модель"] а также при необходимости другие параметры: type="vert или hor" - тип показа; pagesize="количество ссылок на одну страницу". Для блока "Контекстный товар" нужно указать [kt kt_id="номер товарной групп"], а можно указать ID нужной категории (cat_id="XXX"). Все настройки нужно производить в <a href=http://www.mixmarket.biz/doc/partners/goods/programs/>кабинете партнера</a>
+Author:  Партнерская сеть Миксмаркет 
+Version: 1.0
+Author URI: http://www.mixmarket.biz/doc/partners/goods/programs/?from=wordpress
+*/
+
+function gdekupit ( $atts ) {
+	extract(shortcode_atts( array(
+	'gk_id' => '',
+	'brand' => '',
+	'model' => '',
+	'type' => 'vert',
+	'pagesize' => '10',
+	'cat_id' => ''
+	), $atts ));
+	$ref = isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : '';
+	$gk_code = "<script language=\"javascript\" type=\"text/javascript\" src=\"http://".$gk_id.".gk.mixmarket.biz/".$gk_id."/?type=".$type."&pagesize=".$pagesize."&brand=".$brand."&model=".$model."&cat_id=".$cat_id."&div=&r=".$ref."&rnd=".rand(1, 10000)."\" charset=\"windows-1251\"></script>";
+	return $gk_code;
+}
+add_shortcode( 'gdekupit', 'gdekupit' );
+
+function kt ( $atts ) {
+	extract(shortcode_atts( array(
+	'kt_id' => '',
+	'cat_id' => '***'
+	), $atts ));
+	$ref = isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : '';
+	$kt_code = "<script language=\"javascript\" type=\"text/javascript\" src=\"http://".$kt_id.".kt.mixmarket.biz/show/".$kt_id."/div=&cat_id=".$cat_id."&div=&r=".$ref."&rnd=".rand(1, 10000)."\" charset=\"windows-1251\"></script>";
+	return $kt_code;
+}
+add_shortcode( 'kt', 'kt' );
+?>
---
+++ readme.txt
@@ -0,0 +1,28 @@
+=== Mix-Goods (Микс-Товары)===
+Tags: adv, ad, ads, affiliate, mixmarket
+Requires at least: 2.5
+Tested up to: 3.1
+Stable tag: 2.5
+License: GPLv2 or later
+
+Quick and easy installation of advertising Mix-Goods
+Плагин для быстрой и простой установки рекламных кодов Микс-Товары
+
+== Description ==
+
+Quick and easy installation of advertising Mix-Goods
+Плагин для быстрой и простой установки рекламных кодов Микс-Товары
+
+== Installation ==
+
+Upload the Mix-Goods plugin to your blog, Activate it.
+Для блока "Где купить" нужно указать [gdekupit gk_id="номер товарной группы" brand="бренд" model="модель"] а также при необходимости другие параметры: type="vert или hor" - тип показа; pagesize="количество ссылок на одну страницу". 
+Для блока "Контекстный товар" нужно указать [kt kt_id="номер товарной групп"], а можно указать ID нужной категории (cat_id="XXX"). 
+Все настройки нужно производить в <a href=http://www.mixmarket.biz/doc/partners/goods/programs/>кабинете партнера</a>
+
+
+== Changelog ==
+
+= 1.0 = 
+* First version
+
